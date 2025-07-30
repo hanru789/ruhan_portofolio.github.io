10c9ac0fcf3cc466780c3a9927a6517cc8184953
hanru789 <56568893+hanru789@users.noreply.github.com>
2025-07-30 10:28:42 +0700

    Update app.py

--- projec/flip_review_sentiment/app.py
+++ projec/flip_review_sentiment/app.py
@@ -2,3 +2,110 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+
+import re
+import string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+nltk.download('punkt')
+
+def cleaningText(text):
+    text = re.sub(r'@[A-Za-z0-9]+', '', text) # menghapus mention
+    text = re.sub(r'#[A-Za-z0-9]+', '', text) # menghapus hashtag
+    text = re.sub(r'RT[\s]', '', text) # menghapus RT
+    text = re.sub(r"http\S+", '', text) # menghapus link
+    text = re.sub(r'[0-9]+', '', text) # menghapus angka
+    text = re.sub(r'[^\w\s]', '', text) # menghapus karakter selain huruf dan angka
+
+    text = text.replace('\n', ' ') # mengganti baris baru dengan spasi
+    text = text.translate(str.maketrans('', '', string.punctuation)) # menghapus semua tanda baca
+    text = text.strip(' ') # menghapus karakter spasi dari kiri dan kanan teks
+    return text
+
+def casefoldingText(text): # Mengubah semua karakter dalam teks menjadi huruf kecil
+    text = text.lower()
+    return text
+
+def tokenizingText(text): # Memecah atau membagi string, teks menjadi daftar token
+    text = word_tokenize(text)
+    return text
+
+def filteringText(text): # Menghapus stopwords dalam teks
+    listStopwords = set(stopwords.words('indonesian'))
+    listStopwords1 = set(stopwords.words('english'))
+    listStopwords.update(listStopwords1)
+    listStopwords.update(['iya','yaa','gak','nya','na','sih','ku',"di","ga","ya","gaa","loh","kah","woi","woii","woy"])
+    filtered = []
+    for txt in text:
+        if txt not in listStopwords:
+            filtered.append(txt)
+    text = filtered
+    return text
+
+def stemmingText(text): # Mengurangi kata ke bentuk dasarnya yang menghilangkan imbuhan awalan dan akhiran atau ke akar kata
+    # Membuat objek stemmer
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+
+    # Memecah teks menjadi daftar kata
+    words = text.split()
+
+    # Menerapkan stemming pada setiap kata dalam daftar
+    stemmed_words = [stemmer.stem(word) for word in words]
+
+    # Menggabungkan kata-kata yang telah distem
+    stemmed_text = ' '.join(stemmed_words)
+
+    return stemmed_text
+
+def toSentence(list_words): # Mengubah daftar kata menjadi kalimat
+    sentence = ' '.join(word for word in list_words)
+    return sentence
+
+slangwords = {"@": "di", "abis": "habis", "wtb": "beli", "masi": "masih", "wts": "jual", "wtt": "tukar", "bgt": "banget", "maks": "maksimal"}
+def fix_slangwords(text):
+    words = text.split()
+    fixed_words = []
+
+    for word in words:
+        if word.lower() in slangwords:
+            fixed_words.append(slangwords[word.lower()])
+        else:
+            fixed_words.append(word)
+
+    fixed_text = ' '.join(fixed_words)
+    return fixed_text
+
+#####
+
+st.title("Review Sentiment Analysis")
+
+review = st.text_area("Review")
+
+if st.button("Kirim"):
+    st.success(f"Terima kasih, {name}! Pesan Anda telah diterima.")
+
+
+import nltk
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+# Membersihkan teks dan menyimpannya di kolom 'text_clean'
+review = review.apply(cleaningText)
+
+# Mengubah huruf dalam teks menjadi huruf kecil dan menyimpannya di 'text_casefoldingText'
+review = review.apply(casefoldingText)
+
+# Mengganti kata-kata slang dengan kata-kata standar dan menyimpannya di 'text_slangwords'
+review = review.apply(fix_slangwords)
+
+# Memecah teks menjadi token (kata-kata) dan menyimpannya di 'text_tokenizingText'
+review = review.apply(tokenizingText)
+
+# Menghapus kata-kata stop (kata-kata umum) dan menyimpannya di 'text_stopword'
+review = review.apply(filteringText)
+
+# Menggabungkan token-token menjadi kalimat dan menyimpannya di 'text_akhir'
+review = review.apply(toSentence)
+
