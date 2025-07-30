ef61836f4c761e3cc8e6b31108abea745232bd76
hanru789 <56568893+hanru789@users.noreply.github.com>
2025-07-30 10:31:07 +0700

    Update app.py

--- projec/flip_review_sentiment/app.py
+++ projec/flip_review_sentiment/app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
-st.title("Review Sentiment Analysis")
+st.title("Form Kontak")
 
-review = st.text_area("Review")
+name = st.text_input("Nama Anda")
+email = st.text_input("Email")
+message = st.text_area("Pesan")
 
 if st.button("Kirim"):
     st.success(f"Terima kasih, {name}! Pesan Anda telah diterima.")
-
-
-
-
